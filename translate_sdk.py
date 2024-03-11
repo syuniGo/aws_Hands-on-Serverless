6e04686cdae6fed7eb108f5fcef430eaf10faa07
sf <syuni_Go@outlook.com>
2024-03-11 15:31:28 +0900

    test1

--- translate_sdk.py
+++ translate_sdk.py
@@ -1,15 +1,21 @@
 import json
 import boto3
+import logging
 
+logger = logging.getLogger()
+logger.setLevel("INFO")
+    
 translate = boto3.client('translate')
 
 def lambda_handler(event, context):
     input_text = 'おはよう'
+    logger.info(f'## EVENT  {input_text}')
+
     response = translate.translate_text(
+        Text=input_text,
         SourceLanguageCode='ja',
         TargetLanguageCode='en',
     )
-    
     output_text = response.get('TranslatedText')
     return {
         'statusCode': 200,
